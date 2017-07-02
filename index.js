18caad82e42e436bf8602ce2df74faa9a5df3293
Omni <omnipotent_mage@yahoo.com>
2017-07-01 20:24:58 -0700

    Early Progress
    completed env vars and manifest json (I think), more structure to
    redbird

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
+var manifest = require('manifest.json'); //this is how you load stuff right?
 
 var redbird = new require('redbird')({
-  port: 80, 443
+  port: 80,
+  ssl: {
+    port: 443,
+     //certs go here iirc
+    
+    }
 
 
 
@@ -10,4 +16,13 @@ var redbird = new require('redbird')({
 
 
 
+});
+
+redbird.register("YGOSalvation", process.env.SITE_MAIN, {
+	ssl: {
+    letsencrypt: {
+      email: process.env.EMAIL, // Domain owner/admin email
+      //production: true, WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+    }
+  }
 });
