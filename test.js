0c843e4b2f7267b257bb6ff211785f8312975201
Omni <omnipotent_mage@yahoo.com>
2017-07-02 21:31:10 -0700

    Testing pseudocode
    see commit name

---
+++ test.js
@@ -0,0 +1,11 @@
+//describe("") 
+
+//Expectations:
+
+//All of the manifest.json domains should exist, be urls in the form of strings, and be accessible through manifest.domain
+//All of the manifest.json domains should be SSL protected.
+//The node-php-server function should act as Apache for the forum.js
+//The letsencrypt switch should be in the .env file
+//Redbird should load the manifest.json
+//Redbird should run on port 80 for HTTP and 443 for SSL
+//
