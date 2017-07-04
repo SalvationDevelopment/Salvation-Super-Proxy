a49af18d96ed68811922904076eb99c8bd6e3f11
Omni <omnipotent_mage@yahoo.com>
2017-07-03 21:58:36 -0700

    Testing framework 1
    Of 2 or 3, probably.

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-//describe("") 
+//describe(""), assert(), it(), 
 
 //Expectations:
 
@@ -8,4 +8,33 @@
 //The letsencrypt switch should be in the .env file
 //Redbird should load the manifest.json
 //Redbird should run on port 80 for HTTP and 443 for SSL
-//
+var manifest = require('manifest.json');
+var env = require('index.env'); 
+
+describe("manifest", function() {
+    it('should exist for each url', function() {
+      manifest.forEach(
+        manifest.domain != undefined;
+    )});
+    it('should be a valid url', function () {
+      manifest.forEach(
+        typeof manifest.domain = 'string';
+        //node http request goes here
+    )});
+    it('should be SSL protected', function () {
+      manifest.forEach(
+        //node http_s_ request goes here, this function needs to be delayed until all of the other stuff is loaded
+    )});
+});
+
+describe("node-php-server", function () {
+    it('should act as Apache for forum.js', function () {
+        //uhh...need a non-terminal method to do this
+})});
+
+describe(".env file", function () {
+    it('contains the letsencrypt switch as well as the domain email', function() {
+      env.email = process.env.EMAIL; //probably not the right syntax
+})});
+
+//Redbird checks go here
